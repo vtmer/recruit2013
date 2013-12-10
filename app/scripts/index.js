18c48fb31d68bf330400f96ccce2e5db95057e2f
lxjwlt <lxjwlt.c@gmail.com>
2013-12-10 22:52:23 +0800

    初步完成响应式，未写兼容

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,15 @@
 (function($) {
 //    去除no-js类名，js运行成功
 //    $('html').removeClass('no-js');
+
+
+    var $links = $('a[href^=http]');
+
+    $links.on('click', function() {
+        window.open($(this).attr('href'));
+        event.preventDefault();
+    });
+
+
+
 })(jQuery);
