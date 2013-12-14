7d0e30ba3c079d5378989aba676f3e2cee7e928d
hbc <bcxxxxxx@gmail.com>
2013-12-14 15:22:05 +0800

    添加了 console 彩蛋

---
+++ scripts/egg.js
@@ -0,0 +1,1 @@
+!function(){if(window.console){var a=location.origin+"/images/logo.png",b=["background-image: url('"+a+"')","background-repeat: no-repeat","font-size: 30px"].join(";");console.log("%c   ",b),console.log("一个人在看代码？不如和我们一起写代码吧！"),console.log("发邮件给我们： vtmerhome@gmail.com / bcxxxxx+console@gmail.com"),console.log("http://hr.vtmer.com")}}();
